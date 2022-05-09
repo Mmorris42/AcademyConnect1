ab15c476297e221ed9a5c13e1d91f50ef0ac1799
martinique morris <ladawn.marti@gmail.com>
2022-05-09 14:59:59 -0400

    Initial Commit

---
+++ AcademyConnect/AcademyConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyConnectApp.swift
+//  AcademyConnect
+//
+//  Created by Martinique Morris on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AcademyConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
